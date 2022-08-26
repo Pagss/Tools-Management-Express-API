874af89cb79acfc5b36af28d8507c11bd56039d1
Rodrigo Pagano <rpdev22@gmail.com>
2022-08-26 14:25:43 -0300

    controllers 100%

--- Controllers/toolControllers.js
+++ Controllers/toolControllers.js
@@ -1,28 +1,91 @@
 const Tool = require("../Models/toolModel");
+const mongoose = require("mongoose");
 
 //GET all tools
-router.get("/", (req, res) => {
-  res.json({ mssg: "all tools GET" });
-});
+const allTools = async (req, res) => {
+  const tools = await Tool.find({});
+  res.status(200).json(tools);
+};
 
 //POST new tool
-router.post("/", (req, res) => {
+const newTool = async (req, res) => {
   const { title, link, description, tags } = req.body;
-  console.log(title, link, description, tags);
-  res.json({ mssg: req.body });
-});
+
+  // add to the database
+  try {
+    if (tags.length > 8) {
+      res.status(400).json({ error: "tags must contain 8 elements at most" });
+      return;
+    } else {
+      const tool = await Tool.create({ title, link, description, tags });
+      res.status(200).json(tool);
+      return;
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+    return;
+  }
+};
 
 //GET one tool by id
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "one tool GET" });
-});
+const getTool = async (req, res) => {
+  const id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such tool" });
+  }
+
+  const tool = await Tool.findById(id);
+
+  if (!tool) {
+    return res.status(404).json({ error: "No such tool" });
+  }
+
+  res.json(tool);
+};
 
 //UPDATE one tool by id
-router.patch("/:id", (req, res) => {
-  res.json({ mssg: "one tool UPDATE" });
-});
+const updateTool = async (req, res) => {
+  const id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such tool" });
+  }
+
+  try {
+    if (req.body.tags.length > 8) {
+      res.status(400).json({ error: "tags must contain 8 elements at most" });
+      return;
+    } else {
+      const tool = await Tool.findByIdAndUpdate(id, { ...req.body });
+
+      if (!tool) {
+        return res.status(404).json({ error: "No such tool" });
+      }
+      const updatedTool = await Tool.findById(id);
+      res.status(200).json(updatedTool);
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+    return;
+  }
+};
 
 //DELETE one tool by id
-router.delete("/:id", (req, res) => {
-  res.json({ mssg: "one tool DELETE" });
-});
+const deleteTool = async (req, res) => {
+  const id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such tool" });
+  }
+
+  const tool = await Tool.findByIdAndDelete(id);
+
+  if (!tool) {
+    return res.status(404).json({ error: "No such tool" });
+  }
+
+  res.status(200).json({ tool, msg: "Has been deleted" });
+};
+
+module.exports = { allTools, newTool, getTool, updateTool, deleteTool };
--- Routes/tools.js
+++ Routes/tools.js
@@ -2,47 +2,27 @@ const express = require("express");
 
 const router = express.Router();
 
-const Tool = require("../Models/toolModel");
+const {
+  allTools,
+  newTool,
+  getTool,
+  updateTool,
+  deleteTool,
+} = require("../Controllers/toolControllers");
 
 //GET all tools
-router.get("/", (req, res) => {
-  res.json({ mssg: "all tools GET" });
-});
+router.get("/", allTools);
 
 //POST new tool
-router.post("/", async (req, res) => {
-  const { title, link, description, tags } = req.body;
-  console.log(title, link, description, tags);
-
-  // add to the database
-  try {
-    if (tags.length > 8) {
-      res.status(400).json({ error: "tags must have 8 elements at most" });
-      return;
-    } else {
-      const tool = await Tool.create({ title, link, description, tags });
-      res.status(200).json(tool);
-      return;
-    }
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-    return;
-  }
-});
+router.post("/", newTool);
 
 //GET one tool by id
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "one tool GET" });
-});
+router.get("/:id", getTool);
 
 //UPDATE one tool by id
-router.patch("/:id", (req, res) => {
-  res.json({ mssg: "one tool UPDATE" });
-});
+router.patch("/:id", updateTool);
 
 //DELETE one tool by id
-router.delete("/:id", (req, res) => {
-  res.json({ mssg: "one tool DELETE" });
-});
+router.delete("/:id", deleteTool);
 
 module.exports = router;
